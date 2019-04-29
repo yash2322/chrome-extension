b9012d6fbeb22d1db04a44a2eeaefd5de1f2c492
Yash <useryash2322@gmail.com>
2019-04-29 23:34:26 +0530

    up

--- HelloWorld/extension.js
+++ HelloWorld/extension.js
@@ -3,5 +3,13 @@ $(document).ready(function (){
         $(".btn-options").removeClass("active");
         $(this).addClass("active");
     });
+    $("#email-button").click(function () {
+        $("#text").text("Email");
+        $("#input-field").attr("placeholder", "yash@gmail.com");
+    });
+    $("#phone-button").click(function () {
+        $("#text").text("Mobile Number");
+        $("#input-field").attr("placeholder", "950xxxxxxx");
+    });
    
 });
