384545d2c841616a90517bb4fd73638b5e74c445
Yash <useryash2322@gmail.com>
2019-04-30 11:17:59 +0530

    x

--- HelloWorld/pagetitle.js
+++ HelloWorld/pagetitle.js
@@ -1,6 +1,8 @@
-let newheader = document.getElementById('myHeader');
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-  console.log(tabs[0].url);     //url
-  console.log(tabs[0].title);   //title
-  newheader.innerText = tabs[0].title;
+tabs = {};
+tabIds = [];
+focusedWindowId = undefined;
+currentWindowId = undefined;
+chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+    currentWindowId = tabs[0].title;
+    console.log(currentWindowId);
 });
