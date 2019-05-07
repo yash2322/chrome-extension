8f5e45ea79cb5d91db78334d1c59e76c232c8af8
Yash <useryash2322@gmail.com>
2019-05-08 01:01:20 +0530

    added contextMenu

--- HelloWorld/selectusername.js
+++ HelloWorld/selectusername.js
@@ -1,6 +1,14 @@
+//will give option to save when selecting the text. like lastpass
 var context_username={
         "id":"username",
         "title":"Passmein",
         "contexts":["selection"]
 };
 chrome.contextMenus.create(context_username);
+chrome.contextMenus.onClicked.addListener(function(selectedData){
+    if(selectedData.menuItemId=="username" && selectedData.selectionText){
+        //if user has clicked on item with id username i.e passmein and it has selected some text the
+        window.open("https://mayank9804.github.io/PassMeIN/");
+        //chrome.storage.sync.get([variable_for_storage]) : we can use then this API when user logins to save the selected text using 
+    }
+});
